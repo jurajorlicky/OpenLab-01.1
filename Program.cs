0c742d25e79ced1840507e6b7c807c34638d9b5c
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 08:59:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OpenLab_01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int x = Int32.Parse(input);
+            if (x >= 10)
+                Console.WriteLine("This number is greater or equal than 10.");
+            else
+                Console.WriteLine("The number is less then 10");
+        }
+    }
+}
